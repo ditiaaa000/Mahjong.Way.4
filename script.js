f388fef25930d4c86cf58cb343bf4d35b562164d
ditiaaa000 <ditiaaa000@gmail.com>
2026-03-23 20:59:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 
     const RTP_CONFIG = {
         "GACOR":  { scatterChance: 0.05, goldChance: 0.70, baseWinChance: 0.95, maxCascades: 50, weights: { high: 60, mid: 30, low: 10 } },
-        "NORMAL": { scatterChance: 0.04, goldChance: 0.65, baseWinChance: 0.75, maxCascades: 45, weights: { high: 55, mid: 30, low: 20 } },
+        "NORMAL": { scatterChance: 0.04, goldChance: 0.55, baseWinChance: 0.55, maxCascades: 35, weights: { high: 45, mid: 30, low: 20 } },
         "BAD":    { scatterChance: 0.015, goldChance: 0.15, baseWinChance: 0.30, maxCascades: 3, weights: { high: 10, mid: 20, low: 70 } }
     };
     let cfg = RTP_CONFIG[GAME_MODE];
